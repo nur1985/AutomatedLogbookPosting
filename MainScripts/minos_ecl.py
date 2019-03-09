04d31160a115145fdd675051333fc7ec15aab62d
Nuruzzaman <nur.jlab@gmail.com>
2019-03-08 21:28:44 -0600

    Necessary credential for MINOS auto post.
    Necessary credential for MINOS auto post.

---
+++ MainScripts/minos_ecl.py
@@ -0,0 +1,5 @@
+URL = "http://dbweb5.fnal.gov:8080/ECL/minerva"
+password = "xxxx2009"
+user = "minerva_auto"
+SWAPDIR="/daqdata"
+LOGDIR="/home/minos/automated_ecl/logfiles"                                                                                                                                      
